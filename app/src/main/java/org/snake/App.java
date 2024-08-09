6a00fc114a3daa0d69541adb26d721e1823828cc
parzival8888 <parzival8888@proton.me>
2024-08-09 16:40:37 +0800

    two change greetings

--- app/src/main/java/org/snake/App.java
+++ app/src/main/java/org/snake/App.java
@@ -5,7 +5,7 @@ package org.snake;
 
 public class App {
     public String getGreeting() {
-        return "Hello new World!";
+        return "Hello funny World!";
     }
 
     public static void main(String[] args) {
--- app/src/test/java/org/snake/AppTest.java
+++ app/src/test/java/org/snake/AppTest.java
@@ -11,6 +11,6 @@ import static org.junit.Assert.*;
 public class AppTest {
     @Test public void appHasAGreeting() {
         App classUnderTest = new App();
-        assertNotNull("app should have a greeting", classUnderTest.getGreeting());
+        assertNotNull("app must have a greeting", classUnderTest.getGreeting());
     }
 }
