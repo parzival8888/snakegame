c20fd286621ee847d47a247c5e2f6fb54faf73b3
parzival8888 <parzival8888@proton.me>
2024-08-09 16:36:46 +0800

    updated greeting

--- app/src/main/java/org/snake/App.java
+++ app/src/main/java/org/snake/App.java
@@ -5,7 +5,7 @@ package org.snake;
 
 public class App {
     public String getGreeting() {
-        return "Hello Old World!";
+        return "Hello new World!";
     }
 
     public static void main(String[] args) {
