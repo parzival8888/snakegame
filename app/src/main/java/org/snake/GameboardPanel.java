8566a8f9c6e81f14278e73421e00bac30af0e600
parzival8888 <parzival8888@proton.me>
2024-08-21 21:40:13 +0700

    move the snake

--- app/src/main/java/org/snake/GameboardPanel.java
+++ app/src/main/java/org/snake/GameboardPanel.java
@@ -4,6 +4,8 @@ import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 import java.util.ArrayList;
 
 import javax.swing.JPanel;
@@ -17,6 +19,7 @@ public class GameboardPanel extends JPanel {
     private Cell food;
     private int cellSize;
     private int timerInterval;
+    private char direction;
 
     public GameboardPanel(SnakegameModel model) {
         this.model = model;
@@ -35,22 +38,29 @@ public class GameboardPanel extends JPanel {
     @Override
     protected void paintComponent(Graphics graphic) {
         super.paintComponent(graphic);
+        this.setFocusable(true);
+        this.requestFocusInWindow();
+
         if (model.getNewGame()) {
             model.startNewGame();
             prepareBoard(graphic);
             startGame();
             model.setNewGame(false);
-        } 
+        }
         prepareBoard(graphic);
         // Draw the snake head
         graphic.setColor(Color.GREEN);
         graphic.fillRect(snakeHead.getX() * cellSize, snakeHead.getY() * cellSize, cellSize, cellSize);
-    
+
+        // Draw the snake body
+        for (int i = 0; i < snake.size(); i++) {
+            Cell bodySegment = snake.get(i);
+            graphic.fillRect(bodySegment.getX() * cellSize, bodySegment.getY() * cellSize, cellSize, cellSize);
+        }
+
         // Draw the food
         graphic.setColor(Color.RED);
         graphic.fillRect(food.getX() * cellSize, food.getY() * cellSize, cellSize, cellSize);
-
-
     }
 
     private void prepareBoard(Graphics graphic) {
@@ -81,14 +91,42 @@ public class GameboardPanel extends JPanel {
         System.out.println("Snake head positioned at x: " + snakeHead.getX());
 
         // Place a food item randomly on the game board
-        food = model.placeFood();
+        food = model.getFoodLocation();
         System.out.println("Food positioned at x: " + food.getX());
     }
 
+    // First, get the snake direction from the model
+    // Then use a 'switch' statement on direction
+    // If the direction is 'U', adjust the Y coordinate of the snakeHead by
+    // subtracting 1
+    // If the direction is 'R', adjust the X coordinate of the snakeHead by adding 1
+    // etc. for the other directions
+
     private void moveSnake() {
         if (!(snakeHead == null)) {
-            snakeHead.setX(snakeHead.getX() + 1);
-            System.out.println("Moving snake to x: " + snakeHead.getX());    
+            // Move the snake head...see the comments above
+
+            // Move the snake body
+            for (int i = snake.size(); i > 1; i--) {
+                Cell bodySegment = snake.get(i - 1);
+                Cell prevBodySegment = snake.get(i - 2);
+                bodySegment.setX(prevBodySegment.getX());
+                bodySegment.setY(prevBodySegment.getY());
+            }
+
+            // Check for collision between snake head and food
+            // Call the appropriate method on the model to check for a collision
+            // If there is a collision, use the model to place a new food item
         }
     }
+
+    // Handle pressing the arrow keys
+    // First, get the snake direction from the model
+    // Then use a 'switch' statement on the key pressed using e.getKeyCode()
+    // If the left key was pressed, KeyEvent.VK_LEFT, then set the snake direction
+    // to 'L'
+    // But only if it wasn't previously 'R', otherwise the snake will double back on
+    // itself, which isn't allowed
+    // The same for the other arrow keys
+
 }
--- app/src/main/java/org/snake/SnakegameModel.java
+++ app/src/main/java/org/snake/SnakegameModel.java
@@ -10,13 +10,15 @@ public class SnakegameModel {
     private static String configFilename = "snakegame.config";
     private String gameTitle;
     private int boardSize;
-    private int numberofcolumns;
+    private int numberOfColumns;
     private int cellSize;
     private Color boardColour;
     private Color boardGridColour;
     private boolean gameOver;
     private boolean newGame;
     private int timerInterval;
+    private char direction;
+    private int snakeLength;
 
     // The snake, with the head in the first position
     private ArrayList<Cell> snake;
@@ -32,11 +34,13 @@ public class SnakegameModel {
         ConfigReader.readConfig(configFilename);
         this.gameTitle = ConfigReader.getProperty("gametitle");
         this.boardSize = Integer.parseInt(ConfigReader.getProperty("boardsize"));
-        this.numberofcolumns = Integer.parseInt(ConfigReader.getProperty("numberofcolumns"));
-        this.cellSize = this.boardSize / this.numberofcolumns;
+        this.numberOfColumns = Integer.parseInt(ConfigReader.getProperty("numberofcolumns"));
+        this.snakeLength = Integer.parseInt(ConfigReader.getProperty("snakelength"));
+        this.cellSize = this.boardSize / this.numberOfColumns;
         this.boardColour = Color.decode(ConfigReader.getProperty("boardcolour"));
         this.boardGridColour = Color.decode(ConfigReader.getProperty("boardgridcolour"));
-        this.timerInterval =  Integer.parseInt(ConfigReader.getProperty("timerinterval"));
+        this.timerInterval = Integer.parseInt(ConfigReader.getProperty("timerinterval"));
+        this.direction = ConfigReader.getProperty("startdirection").charAt(0);
         System.out.println("game title is: " + this.gameTitle);
         System.out.println("board size is: " + this.boardSize);
         System.out.println("time interval is" + this.timerInterval);
@@ -51,16 +55,21 @@ public class SnakegameModel {
     }
 
     public int getNumberOfColumns() {
-        return numberofcolumns;
+        return numberOfColumns;
     }
+
     public int getTimerInterval() {
         return timerInterval;
+    }
+
+    public int getSnakeLength() {
+        return snakeLength;
     }
 
     public int getCellSize() {
         return cellSize;
     }
- 
+
     public Color getBoardColour() {
         return boardColour;
     }
@@ -77,22 +86,54 @@ public class SnakegameModel {
         return snake;
     }
 
+    public char getDirection() {
+        return direction;
+    }
+
+    public void setDirection(char direction) {
+        this.direction = direction;
+    }
+
+    /**
+     * Check whether two Cells have collided. This could be the
+     * snake head colliding with food, or the snake head colliding
+     * with its own body, or with a wall.
+     * 
+     * A collision occurs when the 'x' and 'y' coordinates of both Cells
+     * are identical.
+     * 
+     * @param cell1 - the first Cell
+     * @param cell2 - the second Cell
+     * @return a boolean indicating whether the two Cells refer to the same location
+     */
+
+
+
+    /**
+     * Initialise the snake with the snake head at position 0 in the ArrayList
+     * 
+     * @return an ArrayList of Cells representing the snake body with the snake head in position 0
+     */ 
     public ArrayList<Cell> initialiseSnake() {
-        int x = randomNumberGenerator.nextInt(boardSize/cellSize);
-		int y = randomNumberGenerator.nextInt(boardSize/cellSize);
-        Cell snakeHead = new Cell(x, y);
+        int x = randomNumberGenerator.nextInt(boardSize / cellSize);
+        int y = randomNumberGenerator.nextInt(boardSize / cellSize);
         snake = new ArrayList<Cell>();
+        Cell snakeHead = new Cell(x, y);
         snake.add(snakeHead);
+        for (int i = 0; i < snakeLength; i++) {
+            Cell bodySegment = new Cell(x, y + i);
+            snake.add(bodySegment); 
+        }
+        System.out.println("Snake length in initialiseSnake is: " + snake.size());
         return snake;
     }
 
     public Cell placeFood() {
-        int x = randomNumberGenerator.nextInt(boardSize/cellSize);
-        int y = randomNumberGenerator.nextInt(boardSize/cellSize);
-        Cell food = new Cell(x, y);
+        int x = randomNumberGenerator.nextInt(boardSize / cellSize);
+        int y = randomNumberGenerator.nextInt(boardSize / cellSize);
+        food = new Cell(x, y);
         return food;
-	}
-
+    }
 
     public void startNewGame() {
         this.initialiseSnake();
